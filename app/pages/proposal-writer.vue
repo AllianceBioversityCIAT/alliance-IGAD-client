32a98b3bcae38ffa95cfd537ace8a9fba3c71d1b
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-09 12:08:28 -0500

    :art: style: Update heading color in proposal-writer.vue
    - Changed the heading color to green using Tailwind CSS classes for improved visibility and aesthetics.

--- app/pages/proposal-writer.vue
+++ app/pages/proposal-writer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold underline text-green-500">
       Hello world from proposal writer
     </h1>
   </div>
