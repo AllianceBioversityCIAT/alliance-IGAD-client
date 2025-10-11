a23e210e8ce6c1915d17ddf1334cd9dc60a3f60b
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-10 20:53:40 -0500

    :sparkles: feat: Add Cloudflare bindings and update API response
    - Introduced a new TypeScript definition file for Cloudflare bindings to enhance type safety.
    - Updated package.json to include a new script for generating types with Wrangler.
    - Modified wrangler.toml to add a new variable for configuration.
    - Enhanced the hello.ts API response to include the new MY_VARIABLE alongside KV data.

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,12 +1,14 @@
 export default defineEventHandler(async (event) => {
   const env = event.context.cloudflare?.env 
   const kv = env?.MY_KV
+  const MY_VARIABLE = env?.MY_VARIABLE
 
   if (!kv) {
     return { 
       hello: "Hello from SSR server api by IGAD",
       error: "KV no disponible" ,
-      kv: kv
+      kv: kv,
+      MY_VARIABLE: MY_VARIABLE
     }
   }
 
