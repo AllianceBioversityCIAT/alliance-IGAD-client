b330753ebfe2c9c50d619de40c3078c2ae0d7e9a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-10 20:27:39 -0500

    :sparkles: feat: kv test added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
 
   modules: [
     '@nuxt/eslint',
--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,16 @@
-export default defineEventHandler((event) => {
-    return {
-      hello: 'Hello from ssr server api by IGAD',
-    }
-  })
+export default defineEventHandler(async (event) => {
+  const env = event.context.cloudflare?.env 
+  const kv = env?.MY_KV as KVNamespace | undefined
+
+  if (!kv) {
+    return { error: "KV no disponible" }
+  }
+
+  // Leer el valor de la clave "data"
+  const value = await kv.get("data")
+
+  return {
+    hello: "Hello from SSR server api by IGAD",
+    kv_data: value
+  }
+})
